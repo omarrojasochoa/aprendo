140f89cb76c5fc933a40878b136dc766b4a81d39
omarrojasochoa <orojas3095@gmail.com>
2022-07-29 16:46:35 -0500

    hola nuevo cd5

--- JavaScript-2/codigo5.js
+++ JavaScript-2/codigo5.js
@@ -16,7 +16,38 @@ const data = [
     ["Omar Rojas","@soyomar"],
     ["Dalto","@soydalto"],
     ["Camila Nesa","@milanesa"],
-    ["Rancio Ramirez","@rancio"],
+    ["Rancio Ramirez"],
 ];
 
 const personas= [];
+for (let i = 0; i < data.length; i++) {
+    personas[i] = new Personas(data[i][0],data[i][1]);
+}
+
+const obtenerPersona=(id,cb)=>{// cb = callback
+    if (personas[id]== undefined){
+        cb("No se encontro la persona");
+    }
+    else{
+        cb(null,personas[id])
+    }
+};
+
+obtenerPersona(0,(err,persona)=>{
+    if (err){
+        console.log(err);
+    }
+    else {
+        console.log(persona.nombre);
+        console.log(obtenerInstagram());
+    }
+})
+
+const obtenerInstagram (id,cb){
+    if (personas[id]== undefined){
+        cb("No se encontro el instagram");
+    }
+    else{
+        cb(null,personas[id].instagram)
+    }
+}
