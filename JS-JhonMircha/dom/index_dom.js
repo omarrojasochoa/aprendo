0fc3699d7841ce5ca647cedd844c0dc392530ddf
omarrojasochoa <orojas3095@gmail.com>
2022-08-15 09:29:23 -0500

    ultimo laptop

--- JS-JhonMircha/dom/index_dom.js
+++ JS-JhonMircha/dom/index_dom.js
@@ -1,15 +1,16 @@
 import hambuguerMenu from "./menu_hambuguesa.js";
 import {digitalClock, alarm} from "./reloj.js";
-import { shortcuts } from "./teclado.js";
+import { moveBall, shortcuts } from "./teclado.js";
 
 const d=document;
 d.addEventListener("DOMContentLoaded",e=>{
     hambuguerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
-    alarm("/JS-JhonMircha/assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+    alarm("../assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
 })
 
 d.addEventListener("keydown",(e)=>{/*Cuando presiono la tecla
 keyup: cuando sueltas la tecla, keypress: mientras lo tengas presionada */
     shortcuts(e);
+    moveBall(e,".ball",".stage")
 })
--- JS-JhonMircha/dom/teclado.js
+++ JS-JhonMircha/dom/teclado.js
@@ -1,10 +1,61 @@
 const d= document;
+let x=0,y=0;
+export function moveBall(e,ball,stage){
+const $ball = document.querySelector(ball);
+const $stage=document.querySelector(stage);    
+limitsBall=$ball.getBoundingClientRect(); //Nos da un objeto de altura anchura, se usara para
+limitsStage=$stage.getBoundingClientRect()    //limitar el movimiento de la bola // y escenario
+
+console.log(e.keyCode)
+console.log(e.key)
+//const move=(direction)=>{}
+switch (e.keyCode) {
+    case 37://izq
+        e.preventDefault();
+        x--;       
+        // move("left")
+        break;
+    case 38://arriba
+        e.preventDefault();
+       y--;
+        // move("up")
+        break;
+    case 39://derecha
+        e.preventDefault();
+        x++;
+       // move("right")
+        break;
+    case 40://abajo
+        e.preventDefault();
+        y++;
+        // move("down")
+        break;
+    default:
+        break;
+};
+
+$ball.style.transform=`translate(${x*10}px,${y*10}px)`;
+
+}
+
 export function shortcuts (e){
-    console.log(e.type)
+   /* console.log(e.type)
     console.log(e.key)
     console.log(e.keyCode)
     console.log(`Ctrl:${e.ctrlKey}`)
     console.log(`Alt:${e.altKey}`)
     console.log(`Shift:${e.shiKey}`)
-    console.log(e)
+    console.log(e)*/
+
+    if(e.key==="a"&&e.altKey){
+        alert("Haz lanzado una alerta en el teclado");
+    }
+    
+    if(e.key==="p"&&e.altKey){
+        prompt("Haz lanzado un aviso con el teclado");
+    }
+    
+    if(e.key==="c"&&e.altKey){
+        confirm("Haz lanzado aviso con el teclado")
+    }
 }
